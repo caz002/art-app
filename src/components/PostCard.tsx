9dee081d4f52717aa1881257fe954730fae7bfd1
caz002 <caz002@ucsd.edu>
2025-07-01 11:06:53 -0700

    added wip page for each post and username changes to match login input

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
 export function PostCard({ src, id, handleFileDelete}: {src: string, id: number, handleFileDelete: (id: number) => void}) {
     return (
     <>
     <div className="relative">
+        <Link to={`/post/${id}`}>
         <img src={src}  width={250} height={250} />
+        </Link>
         <button className="absolute top-5 left-5 w-10 h-10" onClick = {() => handleFileDelete(id)}>x</button>
     </div>
     </>
