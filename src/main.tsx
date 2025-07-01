6ecb0d2a8e0baa8935a7e9baa1131262796526ff
caz002 <caz002@ucsd.edu>
2025-06-30 22:26:47 -0700

    added login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
 import Profile from './pages/Profile.tsx'
+import Login from './pages/Login.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+  },
+]);
+ 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Profile/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
