2be96ec51b120bb672b77c1146a9b05d3a750a30
caz002 <caz002@ucsd.edu>
2025-07-17 19:41:18 -0700

    created navigation bar, added it to home page and profile page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,19 @@
+import MainTitle from "./MainTitle";
+import { useNavigate } from "react-router-dom";
+
 export default function NavBar() {
+    const navigate = useNavigate();
     // TODO: fill out
-    return <></>;
+    return (
+        <div className = "relative flex justify-between items-center w-full container-border p-4 bg-indigo-200">
+            <button className="button" onClick = {() => navigate("/")}><img src="/HouseIcon.svg" alt="house icon"></img> </button>
+            <div className="absolute left-1/2 transform -translate-x-1/2">
+                <MainTitle/>
+            </div>
+            <div className="flex flex-row gap-2">
+                <button className="button" onClick = {() => window.location.href= "https://buymeacoffee.com/"}><img src="/CoffeeIcon.svg" alt="house icon"></img> </button>
+                <button className="button" onClick = {() => navigate("/profile")}><img src="/ProfileIcon.svg" alt="house icon"></img> </button>
+            </div>
+        </div>
+    );
 }
