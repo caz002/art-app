37b798937e989cffa6bcd8fda9af8e333be13da5
caz002 <caz002@ucsd.edu>
2025-07-22 14:49:07 -0700

    updated z-index navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
     const navigate = useNavigate();
     const buttonStyle = "text-xl px-4 py-2 rounded-lg hover:bg-gray-300"
     return (
-        <div className = "fixed top-0 flex justify-between items-center border-b-black border-solid border-b-2 w-full p-8 bg-white">       
+        <div className = "z-10 fixed top-0 flex justify-between items-center border-b-black border-solid border-b-2 w-full p-8 bg-white">       
             <MainTitle/>
             <div className="flex flex-row gap-10">
                 <button className={buttonStyle} onClick = {() => navigate("/")}>Home</button>
