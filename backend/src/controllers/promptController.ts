1039e2424309fa17b20071659095537adbe9410a
Hiroeme <83380500+Hiroeme@users.noreply.github.com>
2025-07-23 19:57:06 -0700

    Remove irrelevant imports

--- backend/src/controllers/promptController.ts
+++ backend/src/controllers/promptController.ts
@@ -1,15 +1,5 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Request, Response } from "express";
 import { fetchGeminiPromptText } from "../utils/gemini";
-
-const apiKey = process.env.GEMINI_API_KEY;
-if (!apiKey) {
-    throw new Error(
-        "Missing Gemini API key configuration in environment variables."
-    );
-}
-
-const genAI = new GoogleGenerativeAI(apiKey);
 
 export const getGeminiPrompt = async (req: Request, res: Response) => {
     try {
