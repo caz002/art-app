48d9575f0dcb2bc7c9bd1d58dea21eacab07e7b7
caz002 <caz002@ucsd.edu>
2025-07-24 09:57:04 -0700

    moved routes to own file and added error page, updated readme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,43 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Home from "./pages/Home.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { DataProvider } from "./contexts/DataContextProvider.tsx";
-import UserProfile from "./pages/UserProfilePage.tsx";
-import CreatePost from "./pages/CreatePostPage.tsx";
-import LoginPage from "./pages/LoginPage.tsx";
-import CreateAccountPage from "./pages/CreateAccountPage.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext.tsx";
-import PrivateRoute from "./components/PrivateRoute.tsx";
+import routes from "./routes/routes.tsx";
 
-// Defines different URL paths for website
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "profile",
-    element: (
-      <PrivateRoute>
-        <UserProfile />
-      </PrivateRoute>
-    ),
-  },
-  {
-    path: "create",
-    element: <CreatePost />,
-  },
-  {
-    path: "login",
-    element: <LoginPage />,
-  },
-  {
-    path: "register",
-    element: <CreateAccountPage />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
