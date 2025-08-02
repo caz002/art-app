477cf0571e352929471bc6f09bbf31a881c05960
Hiroeme <83380500+Hiroeme@users.noreply.github.com>
2025-08-01 18:48:51 -0700

    Feat: Add JWT - Add Login JWT Tokens

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
-import { BadRequestError } from "../utils/errors";
+import { BadRequestError, UserNotAuthenticatedError } from "../utils/errors";
 import { respondWithJSON } from "../utils/json";
-import { hashPassword } from "../utils/auth/password";
-import { addUser } from "../services/userService";
+import { checkHashedPassword, hashPassword } from "../utils/auth/password";
+import { addUser, findUserByEmail } from "../services/userService";
+import { issueJWT } from "../utils/auth/jwt";
 
 export async function handleRegister(req: Request, res: Response) {
     type parameters = {
@@ -14,7 +15,7 @@ export async function handleRegister(req: Request, res: Response) {
     let params: parameters = req.body;
 
     if (!params.email || !params.password || !params.name) {
-        throw new BadRequestError("Missing required email or password!");
+        throw new BadRequestError("Missing required fields.");
     }
 
     const email = params.email;
@@ -30,6 +31,44 @@ export async function handleRegister(req: Request, res: Response) {
         name: newUser.name,
     });
 }
-export async function handleLogin(req: Request, res: Response) {}
+export async function handleLogin(req: Request, res: Response) {
+    type parameters = {
+        email: string;
+        password: string;
+    };
+
+    let params: parameters = req.body;
+
+    if (!params.email || !params.password) {
+        throw new BadRequestError("Missing required fields.");
+    }
+
+    const email = params.email;
+    const user = await findUserByEmail(email);
+
+    // Do we have a user with this email?
+    if (!user) {
+        throw new UserNotAuthenticatedError("Invalid username or password");
+    }
+
+    const password = params.password;
+    const hashedPassword = user.hashedPassword;
+
+    const match = await checkHashedPassword(password, hashedPassword);
+
+    // Does this request's password match with user's password?
+    if (!match) {
+        throw new UserNotAuthenticatedError("Invalid username or password!!");
+    }
+
+    // Return a JWT Token 1HR
+    const accessToken = issueJWT(user.id);
+
+    respondWithJSON(res, 200, {
+        id: user.id,
+        name: user.name,
+        token: accessToken,
+    });
+}
 export async function handleRefreshToken(req: Request, res: Response) {}
 export async function handleLogout(req: Request, res: Response) {}
--- backend/src/utils/auth/jwt.ts
+++ backend/src/utils/auth/jwt.ts
@@ -1,3 +1,46 @@
-export function validateJWT() {}
+import jwt, { Jwt, JwtPayload } from "jsonwebtoken";
+import { config } from "../../config";
+import { UserNotAuthenticatedError } from "../errors";
 
-export function issueJWT() {}
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export function validateJWT(accessToken: string) {
+    const privateKey = config.jwt.secret;
+    let decodedToken: payload;
+    try {
+        decodedToken = jwt.verify(accessToken, privateKey) as payload;
+    } catch (err) {
+        throw new UserNotAuthenticatedError("Invalid Token");
+    }
+
+    const issuer = config.jwt.issuer;
+    if (decodedToken.iss !== issuer) {
+        throw new UserNotAuthenticatedError("Invalid Token");
+    }
+
+    // Do we have a user id?
+    const userId = decodedToken.sub;
+    if (!userId) {
+        throw new UserNotAuthenticatedError("Invalid Token");
+    }
+
+    return userId;
+}
+
+export function issueJWT(userId: string) {
+    const issueAtTime = Math.floor(Date.now() / 1000);
+    const issuer = config.jwt.issuer;
+    const defaultDuration = config.jwt.defaultDuration;
+    const privateKey = config.jwt.secret;
+
+    const jwtToken = jwt.sign(
+        {
+            iss: issuer,
+            sub: userId, // subject
+            iat: issueAtTime,
+            exp: issueAtTime + defaultDuration,
+        },
+        privateKey
+    );
+    return jwtToken;
+}
