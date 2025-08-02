f38e9e580ed4a6b188d458f7becc039b69086cbb
Hiroeme <83380500+Hiroeme@users.noreply.github.com>
2025-08-01 18:48:50 -0700

    Feat: Add ConflictError to addUser

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,4 +1,5 @@
-import { BadGatewayError } from "../utils/errors";
+import { Prisma } from "@prisma/client";
+import { BadGatewayError, ConflictError } from "../utils/errors";
 import prisma from "../utils/prisma";
 
 interface newUser {
@@ -13,6 +14,24 @@ export async function addUser(newUser: newUser) {
             data: newUser,
         });
 
+        return user;
+    } catch (err) {
+        if (err instanceof Prisma.PrismaClientKnownRequestError) {
+            if (err.code === "P2002") {
+                throw new ConflictError("User already exists with this email.");
+            }
+        }
+        throw new BadGatewayError("Upstream database unavailable");
+    }
+}
+
+export async function findUserByEmail(email: string) {
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                email: email,
+            },
+        });
         return user;
     } catch (err) {
         throw new BadGatewayError("Upstream database unavailable");
