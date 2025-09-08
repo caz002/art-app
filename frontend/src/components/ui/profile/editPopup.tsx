f60bc51b71b76fe494a0785553e8fd142bec7eb5
caz002 <caz002@ucsd.edu>
2025-09-08 15:57:22 -0700

    Resolve merge conflicts

---
+++ frontend/src/components/ui/profile/editPopup.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { api } from "@/lib/api";
+import { Label } from "@/components/ui/label";
+import { AnyFieldApi, useForm } from "@tanstack/react-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <>
+      {field.state.meta.isTouched && !field.state.meta.isValid ? (
+        <em>{field.state.meta.errors.join(", ")}</em>
+      ) : null}
+      {field.state.meta.isValidating ? "Validating..." : null}
+    </>
+  );
+}
+export default function EditPopup({
+  setShowEditPopup,
+}: {
+  setShowEditPopup: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const form = useForm({
+    defaultValues: {
+      bio: "",
+      likes: "",
+      occupation: "",
+    },
+    onSubmit: async ({ value }) => {
+      // const res = await api.{}.$post({
+      //   form: {
+      //     bio: value.bio,
+      //     likes: value.likes,
+      //     occupation: value.occupation,
+      //   },
+      // });
+
+      if (!res.ok) {
+        throw new Error("server error");
+      }
+    },
+  });
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-50" />
+      {/* Popup */}
+      <div className="bg-blue-800 rounded-xl shadow-lg p-6 relative z-10 w-11/12 max-w-md">
+        <button
+          className="absolute top-6 right-6 text-gray-500 hover:text-gray-800"
+          onClick={() => setShowEditPopup(false)}
+        >
+          ✕
+        </button>
+        <div className="flex flex-1 flex-col gap-6">
+          <div className="flex flex-1 items-center justify-center font-bold text-4xl">
+            Edit Profile
+          </div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              form.handleSubmit();
+              setShowEditPopup(false);
+            }}
+            className="flex flex-1 flex-col gap-6"
+          >
+            <form.Field
+              name="bio"
+              children={(field) => (
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor={field.name}>Bio</Label>
+                  </div>
+                  <Input
+                    id={field.name}
+                    type="text"
+                    onBlur={field.handleBlur}
+                    onChange={(e) => {
+                      field.handleChange(e.target.value);
+                    }}
+                    defaultValue={field.name}
+                    required
+                  />
+                  <FieldInfo field={field} />
+                </div>
+              )}
+            />
+            <form.Field
+              name="likes"
+              children={(field) => (
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor={field.name}>Likes</Label>
+                  </div>
+                  <Input
+                    id={field.name}
+                    type="text"
+                    onBlur={field.handleBlur}
+                    onChange={(e) => {
+                      field.handleChange(e.target.value);
+                    }}
+                    defaultValue={field.name}
+                    required
+                  />
+                  <FieldInfo field={field} />
+                </div>
+              )}
+            />
+            <form.Field
+              name="occupation"
+              children={(field) => (
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor={field.name}>Occupation</Label>
+                  </div>
+                  <Input
+                    id={field.name}
+                    type="text"
+                    onBlur={field.handleBlur}
+                    onChange={(e) => {
+                      field.handleChange(e.target.value);
+                    }}
+                    defaultValue={field.name}
+                    required
+                  />
+                  <FieldInfo field={field} />
+                </div>
+              )}
+            />
+            <div className="flex flex-1 items-center justify-center">
+              <form.Subscribe
+                selector={(state) => [state.canSubmit, state.isSubmitting]}
+                children={([canSubmit, isSubmitting]) => (
+                  <Button type="submit" disabled={!canSubmit}>
+                    {isSubmitting ? "..." : "Save"}
+                  </Button>
+                )}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes/profile.$userId.tsx
+++ frontend/src/routes/profile.$userId.tsx
@@ -2,123 +2,108 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 import {
-    getPostsByProfileQueryOptions,
-    getSessionQueryOptions,
+  getPostsByProfileQueryOptions,
+  getSessionQueryOptions,
 } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { ProfileGallery } from "@/components/gallery/ProfileGallery";
 import { ProfileSkeleton } from "@/components/skeletons/ProfileSkeleton";
 
 export const Route = createFileRoute("/profile/$userId")({
-    component: Profile,
+  component: Profile,
 });
 
 function Profile() {
-    const { userId } = Route.useParams();
+  const { userId } = Route.useParams();
 
-    // get all information related to user
-    const { data, isPending, isError } = useQuery(
-        getPostsByProfileQueryOptions(userId)
-    );
+  // get all information related to user
+  const { data, isPending, isError } = useQuery(
+    getPostsByProfileQueryOptions(userId)
+  );
 
-    // get session
-    const { data: session } = useQuery(getSessionQueryOptions);
-    // console.log(session);
+  // get session
+  const { data: session } = useQuery(getSessionQueryOptions);
+  // console.log(session);
 
-    if (isError) {
-        return <ProfileSkeleton />;
-    }
+  if (isError) {
+    return <ProfileSkeleton />;
+  }
 
-    if (isPending) {
-        return <ProfileSkeleton />;
-    }
+  if (isPending) {
+    return <ProfileSkeleton />;
+  }
 
-    return (
-        <div className="max-w-4xl m-auto flex flex-col gap-2">
-            <div className="max-w-4xl grid sm:grid-cols-[2fr_1fr] gap-y-4 gap-x-2">
-                <Card>
-                    <CardHeader>
-                        <div className="flex gap-x-4 ">
-                            <img
-                                src={data.user.image}
-                                alt={data.user.name}
-                                className="w-32 h-32 object-cover rounded-md"
-                            />
+  return (
+    <div className="max-w-4xl m-auto flex flex-col gap-2">
+      <div className="max-w-4xl grid sm:grid-cols-[2fr_1fr] gap-y-4 gap-x-2">
+        <Card>
+          <CardHeader>
+            <div className="flex gap-x-4 ">
+              <img
+                src={data.user.image}
+                alt={data.user.name}
+                className="w-32 h-32 object-cover rounded-md"
+              />
 
-                            <div className="flex flex-col gap-y-1">
-                                <div>
-                                    <h1 className="font-bold text-2xl">
-                                        {data.user.name}
-                                    </h1>
-                                </div>
-                                <p>
-                                    hello! this is some placeholder text abt me!
-                                    hello! this is some placeholder text abt me!
-                                    hello!
-                                </p>
-                            </div>
-                        </div>
-                    </CardHeader>
-                </Card>
-                <Card>
-                    <div className="grid grid-cols-2 gap-x-5 gap-y-1  text-xs pr-4 pl-4 m-auto">
-                        <dl className="contents">
-                            <dt className="">Total Posts</dt>
-                            <dd className="text-right font-medium">
-                                {data.posts.length}
-                            </dd>
-                        </dl>
-                        <dl className="contents">
-                            <dt className="">Best Streak</dt>
-                            <dd className="text-right font-medium">
-                                NotImplemented
-                            </dd>
-                        </dl>
-                        <dl className="contents">
-                            <dt className="">Current Streak</dt>
-                            <dd className="text-right font-medium">
-                                NotImplemented
-                            </dd>
-                        </dl>
-                        <dl className="contents">
-                            <dt className="">Perfect Week Count</dt>
-                            <dd className="text-right font-medium">
-                                NotImplemented
-                            </dd>
-                        </dl>
-                        <dl className="contents">
-                            <dt className="">Completion Rate</dt>
-                            <dd className="text-right font-medium">
-                                NotImplemented
-                            </dd>
-                        </dl>
-                    </div>
-                </Card>
+              <div className="flex flex-col gap-y-1">
+                <div>
+                  <h1 className="font-bold text-2xl">{data.user.name}</h1>
+                </div>
+                <p>
+                  hello! this is some placeholder text abt me! hello! this is
+                  some placeholder text abt me! hello!
+                </p>
+              </div>
             </div>
-            <Card>
-                <CardContent>
-                    <div className="m-auto flex gap-x-2">
-                        <p>
-                            <b>Joined </b>
-                            {new Date(data.user.createdAt).toDateString()}
-                        </p>
-                        <p>
-                            <b>Likes </b>
-                            placeholder, placeholder
-                        </p>
-                        <p>
-                            <b>Occupation </b>
-                            placeholder, placeholder
-                        </p>
-                    </div>
-                </CardContent>
-            </Card>
-            <ProfileGallery
-                {...data}
-                session={
-                    session ? { user: { id: session.user.id } } : undefined
-                }
-            />
-        </div>
-    );
+          </CardHeader>
+        </Card>
+        <Card>
+          <div className="grid grid-cols-2 gap-x-5 gap-y-1  text-xs pr-4 pl-4 m-auto">
+            <dl className="contents">
+              <dt className="">Total Posts</dt>
+              <dd className="text-right font-medium">{data.posts.length}</dd>
+            </dl>
+            <dl className="contents">
+              <dt className="">Best Streak</dt>
+              <dd className="text-right font-medium">NotImplemented</dd>
+            </dl>
+            <dl className="contents">
+              <dt className="">Current Streak</dt>
+              <dd className="text-right font-medium">NotImplemented</dd>
+            </dl>
+            <dl className="contents">
+              <dt className="">Perfect Week Count</dt>
+              <dd className="text-right font-medium">NotImplemented</dd>
+            </dl>
+            <dl className="contents">
+              <dt className="">Completion Rate</dt>
+              <dd className="text-right font-medium">NotImplemented</dd>
+            </dl>
+          </div>
+        </Card>
+      </div>
+      <Card>
+        <CardContent>
+          <div className="m-auto flex gap-x-2">
+            <p>
+              <b>Joined </b>
+              {new Date(data.user.createdAt).toDateString()}
+            </p>
+            <p>
+              <b>Likes </b>
+              placeholder, placeholder
+            </p>
+            <p>
+              <b>Occupation </b>
+              placeholder, placeholder
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+      <ProfileGallery
+        {...data}
+        session={session ? { user: { id: session.user.id } } : undefined}
+      />
+    </div>
+  );
 }
