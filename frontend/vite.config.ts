30954e1c7d431b09ee808382a36d317f2cf63720
caz002 <caz002@ucsd.edu>
2025-09-25 14:29:04 -0700

    adjusted settings vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,27 +5,28 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-    plugins: [
-        tanstackRouter({
-            target: "react",
-            autoCodeSplitting: true,
-        }),
-        react(),
-        tailwindcss(),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-            "@backend": path.resolve(__dirname, "../backend"),
-            "@shared": path.resolve(__dirname, "../shared"),
-        },
+  base: "/",
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@backend": path.resolve(__dirname, "../backend"),
+      "@shared": path.resolve(__dirname, "../shared"),
     },
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:3000",
-                changeOrigin: true,
-            },
-        },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
     },
+  },
 });
