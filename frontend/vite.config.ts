d2958d4abe71e0c5e6165c42c3687a72ece635b6
caz002 <caz002@ucsd.edu>
2025-09-26 11:31:36 -0700

    tried adding root to vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+  root: path.resolve(__dirname),
   plugins: [
     tanstackRouter({
       target: "react",
