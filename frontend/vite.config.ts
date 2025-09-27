dc1b9819d0132e15d9d46ddbc2f058e458b6e3a7
caz002 <caz002@ucsd.edu>
2025-09-26 20:19:02 -0700

    attempt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./frontend/src"),
+      "@": path.resolve(__dirname, "./src"),
       "@backend": path.resolve(__dirname, "../backend"),
       "@shared": path.resolve(__dirname, "../shared"),
     },
