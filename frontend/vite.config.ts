449d544cfb04ec6b6f79c50cb4808b3494f1c489
caz002 <caz002@ucsd.edu>
2025-09-26 20:49:35 -0700

    t

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,23 +3,27 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-
+// const routesDir = path.resolve(__dirname, './src/routes');
+// if (!fs.existsSync(routesDir)) {
+//   fs.mkdirSync(routesDir, { recursive: true });
+// }
+// console.log('Routes Directory:', routesDir);
 export default defineConfig({
-  root: path.resolve(__dirname),
+  // root: path.resolve(__dirname),
   plugins: [
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
-      routesDirectory: path.resolve(__dirname, "./src/routes"), // 👈 This is the fix
+      // routesDirectory: path.resolve(__dirname, "./src/routes"), // 👈 This is the fix
     }),
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./frontend/src"),
-      "@backend": path.resolve(__dirname, "./backend"),
-      "@shared": path.resolve(__dirname, "./shared"),
+      "@": path.resolve(__dirname, "./src"),
+      "@backend": path.resolve(__dirname, "../backend"),
+      "@shared": path.resolve(__dirname, "../shared"),
     },
   },
   server: {
