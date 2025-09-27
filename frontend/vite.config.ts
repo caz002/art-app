2539b95ace55cead7fe7877f74cac233ce88d7a7
caz002 <caz002@ucsd.edu>
2025-09-27 15:38:01 -0700

    s

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@backend": path.resolve(__dirname, "../backend"),
-      "@shared": path.resolve(__dirname, "../shared"),
+      "@shared": path.resolve(__dirname, "../shared/"),
     },
   },
   server: {
